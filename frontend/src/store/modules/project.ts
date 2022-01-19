49eb0ccda4a4194da9c4c780b5f0c990b41d550c
Zilong Zhou <529620861@qq.com>
2022-01-19 16:22:00 +0800

    feat: added new type to frontend

--- frontend/src/store/modules/project.ts
+++ frontend/src/store/modules/project.ts
@@ -104,6 +104,8 @@ function convertMember(
       projectMember.relationships!.principal.data,
       includedList
     ),
+    roleProvider: attrs.roleProvider,
+    payload: attrs.payload,
   };
 }
 
